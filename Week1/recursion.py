b21aaa2624cae8e63d638f77d1076ef22236fcd9
dch777 <d.chanana777@gmail.com>
2020-06-04 08:51:43 -0500

    finished recursoin assignment

---
+++ Week1/recursion.py
@@ -0,0 +1,17 @@
+#1
+def sum(x):
+    if x == 1:
+        return 1
+    elif x > 1: 
+        return x + sum(x - 1)
+
+#2
+def fib(x):
+    if x == 0:
+        return 0
+    elif x == 1:
+        return 1
+    else:
+        return fib(x - 1) + fib(x - 2)
+
+#3
