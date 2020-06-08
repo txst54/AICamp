2b33d2c321f252f60568def54617ad0805a3966b
dch777 <d.chanana777@gmail.com>
2020-06-08 16:07:51 -0500

    Added readme for week 2

---
+++ Week2/README.md
@@ -0,0 +1,13 @@
+#Week 2
+
+##Multivariate Gradient Descent
+Edit the degree variable in the gradientDescent.py file to modify the degree, number of points, and learning rate for the program
+
+###Cubic (degree 3)
+![](cubic.png)
+
+###Quadratic (degree 2)
+![](quadratic.png)
+
+###Linear (degree 1)
+![](linear.png)
--- Week2/gradientDescent.py
+++ Week2/gradientDescent.py
@@ -8,7 +8,7 @@ prediction = lambda x, theta: sum([theta[i] * x**i for i in range(len(theta))])
 cost = lambda theta, x, y : sum([((y[i] - prediction(x[i], theta)) ** 2) for i in range(len(x))]) / len(x)
 
 points = 20
-degree = 3
+degree = 1
 alpha = 0.005 / (10**(degree * 2))
 
 theta = [random.random() for i in range(degree + 1)]
@@ -18,7 +18,7 @@ prevCost = cost(theta, X, Y)
 gradient = prevCost
 shouldPlot = 0
 
-while abs(gradient) > .5:
+while abs(gradient) > 1:
     for i in range(len(X)):
         for j in range(len(theta)):
             theta[j] -= alpha * sum([-2 * X[i]**j * (Y[i] - prediction(X[i], theta)) for i in range(len(X))]) / len(X)
