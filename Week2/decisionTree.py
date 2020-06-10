bda365c41bf2529c55f2fc35f183e448957d5f02
dch777 <d.chanana777@gmail.com>
2020-06-10 14:30:58 -0500

    finished decisionTree.py

---
+++ Week2/decisionTree.py
@@ -0,0 +1,24 @@
+import numpy as np
+import math
+import pprint
+
+data = np.loadtxt("agaricus-lepiota.data", delimiter=",", dtype=str)
+data = data.T
+
+Y, X = np.split(data, [1])
+Y = Y[0]
+
+prob = lambda X, x : np.count_nonzero(X == x) / X.shape[0]
+entropy = lambda Y : -1 * np.sum([prob(Y, y) * math.log(prob(Y, y)) for y in np.unique(Y) if y != '?'])
+entropyConditional = lambda X, Y : np.sum([prob(Y, y) * entropy(X[np.where(Y == y)]) for y in np.unique(Y) if y != '?'])
+informationGain = lambda y, x : entropy(y) - entropyConditional(y, x)
+
+def genTree(x, y):
+    if np.unique(y).shape[0] > 1:
+        infGain = [informationGain(y, attr) for attr in x]
+        bestAttr = np.where(infGain == np.max(infGain))[0]
+        return {key:genTree(np.delete(x, bestAttr, 0)[:, x[bestAttr][0] == key], y[x[bestAttr][0] == key]) for key in np.unique(x[bestAttr]) if key != '?'}
+    else:
+        return np.unique(y)
+
+pprint.pprint(genTree(X, Y))
--- Week2/gradientDescent.py
+++ Week2/gradientDescent.py
@@ -10,7 +10,7 @@ prediction = lambda x, theta: sum([theta[i] * x**i for i in range(len(theta))])
 cost = lambda theta, x, y : sum([((y[i] - prediction(x[i], theta)) ** 2) for i in range(len(x))]) / len(x)
 
 points = 20
-degree = 2
+degree = 4
 alpha = 0.005 / (10**(degree * 2))
 
 theta = [random.random() for i in range(degree + 1)]
